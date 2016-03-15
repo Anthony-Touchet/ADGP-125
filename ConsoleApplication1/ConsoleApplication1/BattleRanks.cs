defe252e1035df1a1089de9d86a4060acd0a8eb5
atouchetAIE <cidleaf@gmail.com>
2016-03-15 13:41:38 -0500

    Update: FSM and Basic classes

---
+++ ConsoleApplication1/ConsoleApplication1/BattleRanks.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Inferances;
+using FSM;
+
+namespace BattleRanks
+{
+    class Unit : IStats
+    {
+        public int maxHealth;
+        int _health;
+        int _attack;
+        int _speed;
+        bool _inPar;
+        string _name;
+        double _currExp;
+        double _maxExp;
+        int _level;
+
+        Unit(int a, int h, int s, bool iP, string n, double cEx, double mEx, int l)
+        {
+            attack = a;
+            health = h;
+            maxHealth = h;
+            speed = s;
+            inPar = iP;
+            name = n;
+            currExp = cEx;
+            maxExp = mEx;
+            level = l;
+        }
+
+        public int health
+        {
+            get
+            {
+                return _health;
+            }
+
+            set
+            {
+                _health = value;
+            }
+        }
+
+        public int attack
+        {
+            get
+            {
+                return _attack;
+            }
+
+            set
+            {
+                _attack = value;
+            }
+        }
+
+        public int speed
+        {
+            get
+            {
+                return _speed;
+            }
+
+            set
+            {
+                _speed = value;
+            }
+        }
+
+        public bool inPar
+        {
+            get
+            {
+                return inPar;
+            }
+
+            set
+            {
+                _inPar = value;
+            }
+        }
+
+        public string name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public double currExp
+        {
+            get
+            {
+                return _currExp;
+            }
+
+            set
+            {
+                _currExp = value;
+            }
+        }
+
+        public double maxExp
+        {
+            get
+            {
+                return _maxExp;
+            }
+
+            set
+            {
+                _maxExp = value;
+            }
+        }
+
+        public int level
+        {
+            get
+            {
+                return _level;
+            }
+
+            set
+            {
+                _level = value;
+            }
+        }
+
+        public void CheckLevl()
+        {
+            if(currExp >= maxExp)
+            {
+                currExp = 0;
+                maxExp = maxExp * 1.1;
+                level++;
+            }
+        }
+    }
+
+    class Party : IAttack
+    {
+        List<Unit> team = new List<Unit>();
+        FSM<Enum> turnHandler = new FSM<Enum>(TurnStates.WAIT);
+        Unit currUnit;
+
+        enum TurnStates
+        {
+            WAIT = 0,
+            USE = 1,
+            ATTACK = 2,
+            END = 3,
+        }
+
+        Party()
+        {
+            turnHandler.AddState(TurnStates.USE);
+            turnHandler.AddState(TurnStates.ATTACK);
+            turnHandler.AddState(TurnStates.END);
+            turnHandler.AddTransition(TurnStates.WAIT, TurnStates.USE);
+            turnHandler.AddTransition(TurnStates.USE, TurnStates.ATTACK);
+            turnHandler.AddTransition(TurnStates.ATTACK, TurnStates.USE);
+            turnHandler.AddTransition(TurnStates.ATTACK, TurnStates.END);
+            turnHandler.AddTransition(TurnStates.END, TurnStates.USE);
+            turnHandler.AddTransition(TurnStates.END, TurnStates.WAIT);
+        }
+
+        public void Attack(Party other)
+        {
+            turnHandler.SwitchStates(TurnStates.ATTACK);    //Sets State to Attack
+            team = team.OrderByDescending(o => o.speed).ToList();   //Sorts List by highest to lowest speed
+            currUnit = team[0];
+
+            for (int a = 0; a < other.team.Count; a++)  //Getting a target
+            {
+                if (other.team[team.Count - 1].health > 0)         //Does it have health.
+                {
+                    other.team[team.Count - 1].health -= currUnit.attack;
+                    Console.WriteLine(currUnit.name + " attacks " + other.team[team.Count - 1].name);
+                    Console.WriteLine(other.team[team.Count - 1].name + "'s health is now " + other.team[team.Count - 1].health);
+                    break;
+                }
+            }
+
+            if (team.IndexOf(currUnit) + 1 == team.Count)
+            {
+                currUnit = team[0];
+            }
+
+            else
+            {
+                currUnit = team[team.IndexOf(currUnit) + 1];
+            }
+
+            turnHandler.SwitchStates(TurnStates.END);
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Interfaces.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BattleRanks;
+
+namespace Inferances
+{
+    interface IStats
+    {
+        int health
+        {
+            get;
+            set;
+        }
+
+        int attack
+        {
+            get;
+            set;
+        }
+
+        int speed
+        {
+            get;
+            set;
+        }
+
+        bool inPar
+        {
+            get;
+            set;
+        }
+
+        string name
+        {
+            get;
+            set;
+        }
+
+        double currExp
+        {
+            get;
+            set;
+        }
+
+        double maxExp
+        {
+            get;
+            set;
+        }
+
+        int level
+        {
+            get;
+            set;
+        }
+
+        void CheckLevl();
+    }
+
+    interface IAttack
+    {
+        void Attack(Party other);
+    }
+}
