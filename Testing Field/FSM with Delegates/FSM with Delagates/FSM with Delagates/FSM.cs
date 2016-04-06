48fd3d35732643c638489a24e57538b28011134e
atouchetAIE <cidleaf@gmail.com>
2016-04-06 14:01:02 -0500

    Working On Delegates for FSM

---
+++ Testing Field/FSM with Delegates/FSM with Delagates/FSM with Delagates/FSM.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+
+
+namespace FSM
+{
+    [Serializable]
+    public class FSM <T>   //Finite State Machine.
+    {
+        [Serializable]
+        internal class Link<T>      //Item that holds the transition. THis is a single transition.
+        {
+            public T from;
+            public T to;
+            public Delegate del;
+
+        }
+
+        public T currentState;  //Current State of the FSM
+        private List<T> states;
+        private List<Link<T>> trans;
+     
+        public FSM()
+        {
+
+        }
+
+        public FSM(T state)         //Constructor
+        {
+            states = new List<T>();
+            trans = new List<Link<T>>();
+            AddState(state);
+            currentState = state;                       //Setting the current state to start on.
+        }
+
+        public bool AddState(T state)   //Adding a State to the FSM
+        {
+            if (states.Contains(state))    //Does this FSM already have this state?
+            {
+                //Has state
+                return false;
+            }
+
+            //Does not have this state.
+            states.Add(state);   //Adds state to Dictonary as a Key with a blank set of Transitions.
+            return true;
+        }
+
+        public bool AddTransition(T from, T to, Delegate d) //Add a Transition to the key/state the player is from.
+        {
+            Link<T> temp = new Link<T>();   //Setting up a temp transition variable
+            temp.from = from;
+            temp.to = to;
+            temp.del = d;  
+                
+            if (trans.Contains(temp))  //Does this key/state already have this transition?
+            {
+                //If the transition Exists.
+                return false;
+            }          
+
+            trans.Add(temp);   //Add transition to the list of transitions for that state/key
+            return true;
+        }
+
+        public T SwitchStates(T to)  //Changing the current state of a FSM to another state
+        {
+            Link<T> temp = new Link<T>();   //Set up temp variable
+            temp.from = this.currentState;  //Coming from the current state
+            temp.to = to;
+
+            foreach (Link<T> l in trans)  //Check Transitions for this State/Key
+            {
+                if (l.Equals(temp)) //If Transition Exists, 
+                {
+                    this.currentState = l.to; //Current State equals the next state
+                    if(l.del != null)
+                    {
+                        l.del.DynamicInvoke();
+                    }
+                    
+                    return currentState;
+                }
+            }
+            return currentState;   //Invalid Transition
+        }
+    }
+}
---
+++ Testing Field/FSM with Delegates/FSM with Delagates/FSM with Delagates/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using FSM;
+
+namespace FSM_with_Delagates
+{
+    class Program
+    {
+        static void Attack()
+        {
+            Console.WriteLine("Attack");
+        }
+        static void Use()
+        {
+            Console.WriteLine("Use");
+        }
+        static void End()
+        {
+            Console.WriteLine("End");
+        }
+
+        public enum Turns
+        {
+            WAIT = 0,
+            USE = 1,
+            ATTACK = 2,
+            END = 3,
+        }
+
+        delegate void Del();
+
+        static Del a = Attack;
+        static Del u = Use;
+        static Del e = End;
+
+        static void Main(string[] args)
+        {
+            FSM<Turns> fsm = new FSM<Turns>(Turns.WAIT);
+            fsm.AddState(Turns.USE);
+            fsm.AddState(Turns.ATTACK);
+            fsm.AddState(Turns.END);
+            fsm.AddTransition(Turns.WAIT, Turns.USE, u);
+            fsm.AddTransition(Turns.USE, Turns.ATTACK, a);
+            fsm.AddTransition(Turns.ATTACK, Turns.END, e);
+            fsm.AddTransition(Turns.END, Turns.USE, null);
+
+            fsm.SwitchStates(Turns.USE);
+            fsm.SwitchStates(Turns.ATTACK);
+            fsm.SwitchStates(Turns.END);
+            fsm.SwitchStates(Turns.USE);
+
+            Console.ReadLine();
+        }
+    }
+}
